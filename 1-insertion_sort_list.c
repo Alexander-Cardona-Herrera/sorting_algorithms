814cd1cef915bedd81e25dcdb49002f141308ef4
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-13 16:08:02 -0500

    solving insertion problems

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,11 @@
 #include "sort.h"
 
 /**
-* insertion_sort - sorts an array of integers in ascending order
+* insertion_sort_list - sorts an array of integers in ascending order
 * using the insertiont sort algorithm
 * @list: the array to sort
 * Return: Nothing
 */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = NULL;
@@ -14,6 +13,8 @@ void insertion_sort_list(listint_t **list)
 
 	current = *list;
 	current = current->next;
+	if (listint_t_len(*list) < 2)
+		return;
 	while (current)
 	{
 		if (current->n < current->prev->n)
@@ -47,4 +48,22 @@ void insertion_sort_list(listint_t **list)
 		}
 		current = current->next;
 	}
+}
+
+/**
+ * listint_t_len - eturns the number of elements in a linked dlistint_t list.
+ * @h: pointer of the header of the list
+ * Return: the number of nodes
+ */
+size_t listint_t_len(const listint_t *h)
+{
+	int counter = 0;
+	const listint_t *current = h;
+
+	while (current)
+	{
+		counter++;
+		current = current->next;
+	}
+	return (counter);
 }
