71c7989709ae2e0fe31b8e84fd949bc6ffb4d048
Louvani <paula.louvani@gmail.com>
2021-06-13 21:30:19 -0500

    Quick sort working

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,6 +26,7 @@ void quick_sort(int *array, size_t size)
  * @array: the new array to sort
  * @l: left position of the pivot
  * @r: right position of the pivot
+ * @size: size of the array
  * Return: Nothing
  */
 void q_sort(int *array, int l, int r, size_t size)
@@ -36,39 +37,39 @@ void q_sort(int *array, int l, int r, size_t size)
 	if (l >= r)
 		return;
 	m = partition(array, l, r, size);
-	q_sort(array, m + 1, r, size);
 	q_sort(array, l, m - 1, size);
+	q_sort(array, m + 1, r, size);
 
 }
-
+/**
+ * partition - sdivide the array and return pivot seted
+ * @array: the new array to sort
+ * @l: left position of the pivot
+ * @r: right position of the pivot
+ * @size: size of the array
+ * Return: Nothing
+ */
 int partition(int *array, int l, int r, size_t size)
 {
 	int pivot = array[r];
 	int i = l, j, aux;
 
-	for (; i <= r; i++)
+	for (; i < r; i++)
 	{
-		printf("Primer for i = %d\n", i);
 		if (array[i] >= pivot)
 		{
-			printf("r = %d pivot = %d\n", r, pivot);
-			for (j = i +1; j <= r; j++)
+			for (j = i + 1; j <= r; j++)
 			{
-				printf("Segundo for i = %d j = %d\n", i, j);
-				printf("Segundo for array[i] = %d array[j] = %d\n", array[i], array[j]);
 				if (array[j] <= pivot)
 				{
-					printf("Swaping\n");
 					aux = array[i];
 					array[i] = array[j];
 					array[j] = aux;
 					print_array(array, size);
+					i++;
 				}
 				if (j == r)
-				{
-					printf("retornamos: %d\n", i);
-					return (i);
-				}
+					return (i - 1);
 			}
 		}
 	}
